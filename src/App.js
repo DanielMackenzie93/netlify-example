6720eeef7fed34e1f222c5e2991bff0d38ebb38b
Daniel Mackenzie <danielmackenzie@outlook.com>
2023-05-19 17:15:14 +0100

    chore: updates form

--- src/App.js
+++ src/App.js
@@ -15,11 +15,28 @@ function App() {
         >
           Learn React
         </a>
-        <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-          <input type="text" name="name" />
-          <input type="email" name="email" />
-          <textarea name="message"></textarea>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
+        ,
       </header>
     </div>
   );
